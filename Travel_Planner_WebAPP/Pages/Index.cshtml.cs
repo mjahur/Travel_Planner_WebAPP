0daf089e8bc83bb2bbdce1bd665a49ffe3979f1a
Jahur <Mishu.Jahur@atkinsglobal.com>
2019-11-14 15:15:12 +0000

    Add project files.

---
+++ Travel_Planner_WebAPP/Pages/Index.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Travel_Planner_WebAPP.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
